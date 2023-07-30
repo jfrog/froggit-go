0d60fb3d52c83baaf5cca329b60ccf2b6a6a2e9c
Omer Zidkoni <omerz@jfrog.com>
2023-07-30 16:27:52 +0300

    CR Changes

--- vcsclient/gitlab.go
+++ vcsclient/gitlab.go
@@ -640,7 +640,7 @@ func getProjectOwnerByID(projectID int, client *GitLabClient) (string, error) {
 		}
 	}
 	if project.Namespace == nil {
-		return "", errors.New("could not fetch the name of the project owner. projectID: " + string(projectID))
+		return "", fmt.Errorf("could not fetch the name of the project owner. projectID: %d", projectID)
 	}
 	return project.Namespace.Name, nil
 }
