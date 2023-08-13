c266806016eb38c9066fb9c04bb60d2a999302dd
Omer Zidkoni <omerz@jfrog.com>
2023-08-13 14:02:57 +0300

    fix pr

--- vcsclient/github.go
+++ vcsclient/github.go
@@ -352,15 +352,21 @@ func mapGitHubPullRequestToPullRequestInfo(ghPullRequest *github.PullRequest, wi
 	}
 
 	var sourceRepoName, sourceRepoOwner string
-	if ghPullRequest.Head.Repo != nil && ghPullRequest.Head.Repo.Owner != nil {
-		sourceRepoName = vcsutils.DefaultIfNotNil(ghPullRequest.Head.Repo.Name)
-		sourceRepoOwner = vcsutils.DefaultIfNotNil(ghPullRequest.Head.Repo.Owner.Login)
+	if ghPullRequest.Head.Repo == nil {
+		return PullRequestInfo{}, errors.New("the target repository information is missing when fetching the pull request details")
 	}
+	if ghPullRequest.Head.Repo.Owner == nil {
+		return PullRequestInfo{}, errors.New("the target repository owner name is missing when fetching the pull request details")
+	}
+	sourceRepoName = vcsutils.DefaultIfNotNil(ghPullRequest.Head.Repo.Name)
+	sourceRepoOwner = vcsutils.DefaultIfNotNil(ghPullRequest.Head.Repo.Owner.Login)
 
 	var targetRepoName, targetRepoOwner string
-	if ghPullRequest.Base.Repo != nil && ghPullRequest.Base.Repo.Owner != nil {
-		targetRepoName = vcsutils.DefaultIfNotNil(ghPullRequest.Base.Repo.Name)
-		targetRepoOwner = vcsutils.DefaultIfNotNil(ghPullRequest.Base.Repo.Owner.Login)
+	if ghPullRequest.Base.Repo == nil {
+		return PullRequestInfo{}, errors.New("the source repository information is missing when fetching the pull request details")
+	}
+	if ghPullRequest.Base.Repo.Owner == nil {
+		return PullRequestInfo{}, errors.New("the source repository owner name is missing when fetching the pull request details")
 	}
 
 	var body string
