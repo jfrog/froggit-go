cfbcc8175acc64ed5e35af759ee9a7b1c9685c4f
Eran Turgeman <erant@jfrog.com>
2025-07-17 17:44:22 +0300

    .

--- vcsclient/github_test.go
+++ vcsclient/github_test.go
@@ -854,6 +854,7 @@ func TestGitHubClient_UploadScanningAnalysis(t *testing.T) {
 		fmt.Sprintf("/repos/%s/%s/commits?page=1&per_page=50&sha=master", owner, repo1), createGitHubSarifUploadHandler)
 	defer cleanUp()
 
+	// TODO eran - the updated UploadSarif that is used in UploadCodeScanning is not returnind SarifId
 	sarifID, err := client.UploadCodeScanning(ctx, owner, repo1, "master", scan)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedUploadSarifID, sarifID)
